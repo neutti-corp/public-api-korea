11941a8afa2683a49932ea7ba5ca275bbc943372
Park.C.S <parkcs@neutti.com>
2024-04-25 23:59:35 +0900

    api 정비

--- src/test/java/com/neutti/npa/AppTest.java
+++ src/test/java/com/neutti/npa/AppTest.java
@@ -19,7 +19,7 @@ import java.util.Map;
  */
 @Slf4j
 public class AppTest extends TestCase {
-    public void testSomething() {
+    public void test() {
         DataApiService<AedInfo> service = DataApiService.getInstance();
         service.setPath("/B552657/AEDInfoInqireService/getAedLcinfoInqire");
         service.setServiceKey("maQN9ERlzOBZfcjIu1K8huRRCi+YhF++eEy+tnCMTi3QGADZlvLzq/YgO2t3O95nzI5MGT5dkNmx03gEAnzqyA==");
