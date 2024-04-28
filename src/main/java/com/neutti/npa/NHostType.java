d1a7b9d1c8b7858a488179e54cce7f4dfa0f2ebe
Park.C.S <parkcs@neutti.com>
2024-04-28 19:37:39 +0900

    API 호스트 타입 정의

--- src/main/java/com/neutti/npa/NHostType.java
+++ src/main/java/com/neutti/npa/NHostType.java
@@ -1,5 +1,28 @@
 package com.neutti.npa;
 
 public enum NHostType {
-    DATA_GO, DATA_GG, DATA_SEOUL, VWORLD, MOLIT, EXIM
+    /**
+     * <a href="https://www.data.go.kr/">공공데이터포털</a>
+     */
+    DATA_GO,
+    /**
+     * <a href="https://data.gg.go.kr/">경기데이터드림</a>
+     */
+    DATA_GG,
+    /**
+     * <a href="https://data.seoul.go.kr/">서울열린데이터광장</a>
+     */
+    DATA_SEOUL,
+    /**
+     * <a href="https://www.vworld.kr/dev/v4api.do">디지털트윈국토</a>
+     */
+    VWORLD,
+    /**
+     * <a href="http://openapi.molit.go.kr/">국토교통부</a>
+     */
+    MOLIT,
+    /**
+     * <a href="https://www.koreaexim.go.kr/ir/HPHKIR019M01">한국수출입은행</a>
+     */
+    EXIM,
 }
